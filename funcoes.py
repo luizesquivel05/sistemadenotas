240d017353dc1a10aad28f5b7796aaca938b439f
Luiz Esquivel <62815987+luizesquivel05@users.noreply.github.com>
2023-01-08 21:27:04 -0300

    organizar funcoes

--- funcoes.py
+++ funcoes.py
@@ -1,12 +1,6 @@
+gabarito = input('Digite o gabarito da prova: ').lower() # leitura do gabarito e o deixa em minúsculo.
+qtdAlunos = int(input('Digite a quantidade de alunos: ')) # leitura da quantidade de alunos.
 dados = list() # lista para armazenar informações dos estudantes.
-
-# funcao para calcular a notas:
-def calcularNota(resp, gaba):
-    nota = 0
-    for x in range(len(gaba)):
-        if resp[x] == gaba[x]:
-            nota += 1
-    return nota
 
 # funcao para receber as informações dos estudantes e envia para a lista:
 def infoAlunos(qtdAlunos, gabarito):
@@ -16,3 +10,11 @@ def infoAlunos(qtdAlunos, gabarito):
         resp = str(input('Resposta do aluno: ').lower())
         nota = calcularNota(resp, gabarito)
         dados.append({'nome': nome, 'matricula': matricula, 'nota': nota})
+
+# funcao para calcular a notas:
+def calcularNota(resp, gaba):
+    nota = 0
+    for x in range(len(gaba)):
+        if resp[x] == gaba[x]:
+            nota += 1
+    return nota
