e6cecefccdbceba969b8a8535d49e9442667f753
Luiz Esquivel <62815987+luizesquivel05@users.noreply.github.com>
2023-01-08 21:45:57 -0300

    funcao para ordenar notas

--- funcoes.py
+++ funcoes.py
@@ -2,7 +2,7 @@ gabarito = input('Digite o gabarito da prova: ').lower() # leitura do gabarito e
 qtdAlunos = int(input('Digite a quantidade de alunos: ')) # leitura da quantidade de alunos.
 dados = list() # lista para armazenar informações dos estudantes.
 
-# funcao para receber as informações dos estudantes e envia para a lista:
+# funcao que irá receber as informações dos estudantes e envia para a lista:
 def infoAlunos(qtdAlunos, gabarito):
     for i in range(qtdAlunos):
         matricula = int(input('Matricula do Aluno: '))
@@ -11,7 +11,7 @@ def infoAlunos(qtdAlunos, gabarito):
         nota = calcularNota(resp, gabarito)
         dados.append({'nome': nome, 'matricula': matricula, 'nota': nota})
 
-# funcao para calcular a notas:
+# funcao que irá calcular a notas:
 def calcularNota(resp, gaba):
     nota = 0
     for x in range(len(gaba)):
@@ -19,8 +19,14 @@ def calcularNota(resp, gaba):
             nota += 1
     return nota
 
-# funcao usara para ordenar a lista com os nomes dos estudantes:
+# função que usará para ordenar a lista com os nomes dos estudantes:
 def ordenarNome(qtdAlunos):
     for i in range(qtdAlunos):
-        ordenar = sorted(dados, key=lambda row: row['nome']) # esse comando lambda realiza uma filtragem das linhas a partir da coluna.
+        ordenar = sorted(dados, key=lambda row: row['nome']) # esse comando lambda que realizará uma filtragem das linhas a partir da coluna nome.
+        print(ordenar[i])
+
+# função que irá ser usada para ordenar a lista de informações dos estudantes a partir da ordem crescente de notas:        
+def ordenarNota(qtdAlunos):
+    for i in range(qtdAlunos):
+        ordenar = sorted(dados, key=lambda row: row['nota']) # esse comando lambda que realizará filtragem de linhas a partir da coluna nota.
         print(ordenar[i])
