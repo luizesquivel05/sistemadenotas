3679e863a1ddb3cbbc468c512794e0b5cb955340
Luiz Esquivel <62815987+luizesquivel05@users.noreply.github.com>
2023-01-08 22:40:56 -0300

    organizacao do codigo e sua divisao

--- index.py
+++ index.py
@@ -1,25 +1,12 @@
 import funcoes as fn
-
-# gabarito: abdebaabcd
-
+import texto as txt
 
 gabarito = input('Digite o gabarito da prova: ').lower()
 qtdAlunos = int(input('Digite a quantidade de alunos: '))
-dados = list()
 info = fn.infoAlunos(qtdAlunos, gabarito)
 loop = 1
 while loop == 1:
-    print('\n')
-    print('Selecione uma das opções abaixo:\n')
-    print('Digite 1 para ver a lista em ordem alfabética dos alunos com o número de matricula e a sua nota.\n')
-    print('Digite 2 para ver a lista em ordem crescente de notas com o nome do aluno, o numero da matricula e a sua nota.\n')
-    print('Digite 3 para ver a lista em ordem crescente de notas com o nome do aluno, o numero da matricula e a sua nota para os alunos aprovados (>= 7.0).\n')
-    print('Digite 4 para ver a lista em ordem decrescente de notas com o nome do aluno, o numero da matricula e a sua nota para os alunos reprovados (< 7.0).\n')
-    print('Digite 5 para ver o percentual de aprovação, sabendo-se que a nota mínima para aprovação é >= 7.0.\n')
-    print('Digite 6 para ver a nota que teve a maior frequência absoluta.\n')
-    print('Digite 7 para ver o aluno com a maior nota (nome, matricula e nota).\n')
-    print('Digite 8 para ver o aluno com a menor nota (nome, matricula e nota).\n')
-    print('Digite 9 para ver a media da turma.\n')
+    print(txt.menu())
     opcao = int(input())
     print('\n')
     if opcao == 1:
